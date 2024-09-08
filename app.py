ade6427db34a2ad6ca13c5e920640841d9510639
Arthur63 <arthur63@163.com>
2024-09-08 20:35:41 +0800

    add log

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from enum import Enum
 import psycopg
+from psycopg.rows import dict_row
 
 conn_info = {
     "user": "arthur",
@@ -17,9 +18,12 @@ class Res(Enum):
     DUPLICATE = 2
     INCOMPLETE = 3
 
+    NOUSER = 4
+    WRONGPASSWD = 5
 
-def res_wrap(res):
-    return {"code": res.value, "data": res.name}
+
+def res_wrap(res, data=None):
+    return {"code": res.value, "data": data if data else res.name}
 
 
 app = Flask(__name__)
@@ -71,6 +75,54 @@ def student_reg():
         return res_wrap(Res.INTERNAL)
 
     return res_wrap(Res.OK)
+
+
+@app.route("/teacher/log", methods=["POST"])
+def teacher_log():
+    data = request.form
+    if not data_check(data, "username", "password"):
+        return res_wrap(Res.INCOMPLETE)
+
+    try:
+        sql = "SELECT * FROM teacher WHERE username = %s"
+        value = (data["username"],)
+        with conn.transaction():
+            with conn.cursor(row_factory=dict_row) as cur:
+                cur.execute(sql, value)
+                if not (user := cur.fetchone()):
+                    return res_wrap(Res.NOUSER)
+                if data["password"] != user["password"]:
+                    return res_wrap(Res.WRONGPASSWD)
+                del user["username"]
+                del user["password"]
+    except Exception as e:
+        return res_wrap(Res.INTERNAL)
+
+    return res_wrap(Res.OK, user)
+
+
+@app.route("/student/log", methods=["POST"])
+def student_log():
+    data = request.form
+    if not data_check(data, "username", "password"):
+        return res_wrap(Res.INCOMPLETE)
+
+    try:
+        sql = "SELECT * FROM student WHERE username = %s"
+        value = (data["username"],)
+        with conn.transaction():
+            with conn.cursor(row_factory=dict_row) as cur:
+                cur.execute(sql, value)
+                if not (user := cur.fetchone()):
+                    return res_wrap(Res.NOUSER)
+                if data["password"] != user["password"]:
+                    return res_wrap(Res.WRONGPASSWD)
+                del user["username"]
+                del user["password"]
+    except Exception:
+        return res_wrap(Res.INTERNAL)
+
+    return res_wrap(Res.OK, user)
 
 
 if __name__ == "__main__":
---
+++ test_log.sh
@@ -0,0 +1,15 @@
+http -f :5000/teacher/log \
+    username='lilaoshi' \
+    password='12345'
+
+http -f :5000/teacher/log \
+    username='wanglaoshi' \
+    password='12345'
+
+http -f :5000/teacher/log \
+    username='lilaoshi' \
+    password='54321'
+
+http -f :5000/student/log \
+    username='zhangsan' \
+    password='12345'
